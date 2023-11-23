57a58373f8acffba5e83f2ff3e60f3b06f7c7b45
Kiweeks <dan.xpom@bk.ru>
2023-11-23 16:30:01 +0300

    appdate

--- parsing_dver_com_clone/testing.py
+++ parsing_dver_com_clone/testing.py
@@ -17,8 +17,6 @@ bs = BeautifulSoup(resp.text, 'lxml')
 title = bs.find('h1').text.strip()
 result += title + '\n\n'
 
-# result += f'{bs.find("div", class_="_headline").text}: {bs.find("span", class_="_value").text}' + '\n\n'
-#
 sides = bs.find('td', id='size_selecting').find_all('div')
 
 for side in sides:
@@ -29,17 +27,26 @@ result += '\n\n'
 params = bs.find('table', id='opisanie_table').find_all('tr')
 
 for param in params:
-    # name = param.find('div', class_='list-title').text.strip()
-    # value = param.find('div', class_='list-value').text.strip()
-    # result += f"{name} {value}\n"
-    print(param.text)
+    if param.find('strong') is None and param.find('td'):
+        continue
+    name = param.find('strong').text.strip()
+    value = param.find_all('td')[1].text.strip()
+    result += f"{name}: {value}\n"
 
-# photos = bs.find('div', class_='_slider').find_all('a')
+# colors = bs.find_all()
+
+# for color in colors:
+#     # pass
+#     if 'title' in  color:
+#         print(color)result += f'{bs.find("div", class_="_headline").text}: {bs.find("span", class_="_value").text}' + '\n\n'
+
+
+# photos = bs.find_all('td', valign="top")
 # files = []
 # for photo in photos:
 #     full_path = 'https://' + get_domain(link) + f'/{photo.find("img")["src"]}'
 #     name = photo.find("img")["src"].split('/')[-1]
-#
+#     print(photo)
 #     files.append({'name': name, 'data': requests.get(full_path).content})
 
-print(params)
+print(result)
