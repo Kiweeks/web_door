541abbcc4a219e428f28d83d8c40cb548ee1c526
Kiweeks <dan.xpom@bk.ru>
2023-12-16 15:56:25 +0300

    creat flower web

--- kiweeks/main/admin.py
+++ kiweeks/main/admin.py
@@ -1,11 +1,5 @@
 from django.contrib import admin
-from .models import Door, Color_inside, Color_outside, Photo_door, Type_door, Size_door, Category_door, Side_of_door
+from .models import Item, Image
 
-admin.site.register(Door)
-admin.site.register(Color_outside)
-admin.site.register(Color_inside)
-admin.site.register(Photo_door)
-admin.site.register(Type_door)
-admin.site.register(Size_door)
-admin.site.register(Category_door)
-admin.site.register(Side_of_door)
+admin.site.register(Item)
+admin.site.register(Image)
--- kiweeks/main/views.py
+++ kiweeks/main/views.py
@@ -3,7 +3,7 @@ from django.shortcuts import render, HttpResponse
 from django.db.models import Prefetch
 import pickle
 from django.views.generic import ListView
-from .models import Door, Color_outside, Color_inside, Size_door, Category_door
+from .models import Image, Item
 
 
 # class DoorIndex(ListView):
@@ -12,33 +12,12 @@ from .models import Door, Color_outside, Color_inside, Size_door, Category_door
 #     context_object_name = 'doors'
 
 def index(request):
-    doors = Door.objects.prefetch_related(Prefetch('sizes', queryset=Size_door.objects.all()),
-                                          Prefetch('colors_inside', queryset=Color_inside.objects.all()),
-                                          Prefetch('colors_outside', queryset=Color_outside.objects.all()))
-    paginator = Paginator(doors, 48)
-
-    page_number = request.GET.get('page')
-    page_obj = paginator.get_page(page_number)
-    # for door in doors:
-        # print(len(door.photo_door_set.all()))
-        # for photo in door.photo_door_set.all:
-        #     print(photo)
-    return render(request, 'main/index.html', context={'page_obj':page_obj, 'doors': doors})
+    pass
 
 
 def door_info(request, door_id):
-    door = Door.objects.get(id=door_id)
-    # for color in door.colors_inside.iterator():
-    #     print(color.color, color.code)
-    return render(request, 'main/shablon_door.html', context={'door': door})
+    pass
 
 
 def assortment(request, category):
-    category = Category_door.objects.get(link_text=category)
-    doors = Door.objects.filter(category_id=category.id)
-
-    paginator = Paginator(doors, 20)
-
-    page_number = request.GET.get('page')
-    page_obj = paginator.get_page(page_number)
-    return render(request, 'main/entrance_doors.html', context={'page_obj': page_obj, 'doors': doors, 'category': category})
+    pass
