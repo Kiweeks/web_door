05c208bc669f403f3a17754afb8cc57a64089207
Kiweeks <dan.xpom@bk.ru>
2023-12-27 01:17:52 +0300

    Page update

--- kiweeks/main/views.py
+++ kiweeks/main/views.py
@@ -17,13 +17,13 @@ def index(request):
                                           Prefetch('colors_outside', queryset=Color_outside.objects.all()))
     paginator = Paginator(doors, 48)
 
-    page_number = request.GET.get('page')
+    page_number = request.GET.get('page', '1')
     page_obj = paginator.get_page(page_number)
     # for door in doors:
         # print(len(door.photo_door_set.all()))
         # for photo in door.photo_door_set.all:
         #     print(photo)
-    return render(request, 'main/index.html', context={'page_obj':page_obj, 'doors': doors})
+    return render(request, 'main/index.html', context={'page_obj':page_obj, 'doors': doors, 'current_page': int(page_number)})
 
 
 def door_info(request, door_id):
@@ -39,6 +39,6 @@ def assortment(request, category):
 
     paginator = Paginator(doors, 20)
 
-    page_number = request.GET.get('page')
+    page_number = request.GET.get('page','1')
     page_obj = paginator.get_page(page_number)
-    return render(request, 'main/entrance_doors.html', context={'page_obj': page_obj, 'doors': doors, 'category': category})
+    return render(request, 'main/entrance_doors.html', context={'page_obj': page_obj, 'current_page': int(page_number), 'doors': doors, 'category': category})
